3a98620a762d9a07ef98dceeaf404f42a03a9b77
EC2 Default User <ec2-user@ip-172-31-40-233.ec2.internal>
2020-01-11 16:48:45 +0000

    planning.html form completed and map add and google api linked up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,9 @@
-$(document).ready(function(){
- 
- 
- 
-     
-     
-
- 
- 
- 
-   
-   
-    
-});
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 2.5,
+        center: {
+            lat: 51.5074,
+            lng: 0.1278
+        }
+    });
+}
